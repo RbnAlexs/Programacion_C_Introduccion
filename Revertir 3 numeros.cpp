5e74e2694a902745541ef85521ef8d49dd711710
Ruben Sanchez <rsanchezcolin@icloud.com>
2016-12-27 13:34:11 -0600

    Revertir 3 numero ingresados

---
+++ Revertir 3 numeros.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+main(){
+       int numero[3];
+       float promediog;
+       printf(" Revertir tres numeros: ");
+       for(int i=1;i<4;i=i+1){
+       printf("\n\tIngresa el numero %i: ",i);
+       scanf("%i",&numero[i-1]);}
+    
+      printf("\n\t       Los numeros son: %i, %i, %i ",numero[2],numero[1],numero[0]);
+      
+       
+       
+              getch();}
